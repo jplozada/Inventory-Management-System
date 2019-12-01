653b663e78e520ad688132ab6ca156a4b410a557
jplozada <juxnpx@gmail.com>
2019-12-01 12:06:55 -0400

    First commit: Database conection implemented, Categoria class connected to sql server stored procedures

---
+++ Sistema.Datos/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class Conexion
+    {
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+
+        }
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Data Source=DESKTOP-CKVRU3K;Initial Catalog=dbsistema;Integrated Security=True";
+            }
+            catch(Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+        public static Conexion getInstancia()
+        {
+            if(Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
